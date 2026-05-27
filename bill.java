26d55178b6e8cd8713b5429111fce2da3097ba2b
Nandini <vadigalanandini@gmail.com>
2026-05-27 10:43:57 +0530

    Add files via upload

---
+++ bill.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class bill
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+        	int n=s.nextInt();
+        	if(n>250)
+		{
+            		System.out.println(n-250);
+        	}
+        	else
+		{
+            		System.out.println("don't pay");
+        	}
+        
+	}
+}
---
+++ pyramid.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class pyramid 
+{
+    public static void main(String[] args) 
+    {
+    	Scanner s = new Scanner(System.in);
+    	int n = s.nextInt();
+    	for (int i = 1; i <= n; i++) 
+	{
+    		for (int j = 1; j <= n-i; j++) 
+        	{
+        		System.out.print(" ");
+    		}
+		for(int k=1;k<=(2*i-1);k++)
+		{
+
+   	         	System.out.print("*");
+		}
+		System.out.println();
+	}
+    }
+}
